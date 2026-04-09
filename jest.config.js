f0c9c08306f55f00780cce484876f6935fbc60fe
Lomintun Suwito <lsuwito@beyondtrust.com>
2026-04-08 17:54:50 -0700

    fix linter errors

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   roots: ['<rootDir>/src/', '<rootDir>/test/'],
   transform: {
-    '^.+\\.[jt]sx?$': 'ts-jest',
+    '^.+\\.[jt]sx?$': 'ts-jest'
   },
   testRegex: '(/__tests__/.*|\\.(test|spec))\\.[tj]sx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testEnvironment: 'node',
   moduleNameMapper: {
     '^@actions/([^/]+)$': '<rootDir>/node_modules/@actions/$1',
-    '^@actions/([^/]+)/(.*)$': '<rootDir>/node_modules/@actions/$1/$2',
+    '^@actions/([^/]+)/(.*)$': '<rootDir>/node_modules/@actions/$1/$2'
   },
   transformIgnorePatterns: [
-    'node_modules/(?!@actions/)',
-  ],
-};
+    'node_modules/(?!@actions/)'
+  ]
+}
