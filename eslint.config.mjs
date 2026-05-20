a573c008c2e34fb89f5a36f3c52537809604fe52
Lomintun Suwito <lsuwito@beyondtrust.com>
2026-05-20 09:43:09 -0700

    fix brace-expansion vulnerability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import typescriptEslint from "typescript-eslint";
 import globals from "globals";
 import tsParser from "@typescript-eslint/parser";
 import path from "node:path";
